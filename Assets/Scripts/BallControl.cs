4f4320a15d29a933ff2293a34ffc599d01b48678
HertonJP <125948356+HertonJP@users.noreply.github.com>
2023-03-14 12:40:05 +0700

    Add files via upload

---
+++ Assets/Scripts/BallControl.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+
+    private Rigidbody2D rb2d;
+    public float rand;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        // GoBall();
+        Invoke("GoBall", 2);
+    }
+
+    void GoBall()
+    {
+         rand = Random.RandomRange(2, 10);
+        if (rand < 1) rb2d.AddForce(new Vector2(20, -15));
+        else rb2d.AddForce(new Vector2(-20, -15));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
